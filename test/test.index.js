75701c95a7a71ff9f6efeb2865ecea58afd70885
Jennifer Fong <jen@ednapiranha.com>
2014-09-10 11:14:27 -0700

    fixed tests

--- test/test.index.js
+++ test/test.index.js
@@ -8,7 +8,7 @@ var validator = require('../index');
 
 describe('validator', function () {
   it('should validate a non-existent manifest with errors', function (done) {
-    validator('test/invalid.webapp', false, function (err, results) {
+    validator('test/invalid.webapp', {}, function (err, results) {
       should.exist(err);
       err.toString().should.equal("Error: ENOENT, open 'test/invalid.webapp'");
       done();
@@ -16,7 +16,7 @@ describe('validator', function () {
   });
 
   it('should validate a minimal manifest and icon with no errors', function (done) {
-    validator('test/manifest.webapp', false, function (err, results) {
+    validator('test/manifest.webapp', {}, function (err, results) {
       results.errors.should.be.empty;
       results.warnings.should.be.empty;
       done();
@@ -24,7 +24,7 @@ describe('validator', function () {
   });
 
   it('should validate a minimal manifest and icon with some errors', function (done) {
-    validator('test/manifest-error.webapp', false, function (err, results) {
+    validator('test/manifest-error.webapp', {}, function (err, results) {
       results.errors.MandatoryFieldName.should.equal('Mandatory field name is missing');
       results.errors.InvalidIconSize.should.equal('The icon must be a square');
       results.warnings.should.be.empty;
